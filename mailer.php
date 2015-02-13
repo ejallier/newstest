eb1ac38f6fee45491e584bc5afad2214d0d073f6
ejallier <ejallier@cefim-formation.net>
2015-02-13 17:00:37 +0100

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -8,12 +8,12 @@ function get_include_contents($filename) {
     return false;
 }
 
-$to      = 'ejallier@cefim.eu';
+$to      = 'toto@mail.com';
 //Vos adresses de test
 $subject = 'Un nouveau pack pour vous !';
 $message = get_include_contents('index.html');
-$headers = "From: \"SSW\"<ssw@erwannjallier.com>\n"; //Taper ici votre adresse mail
-$headers .= "Reply-To: ssw@erwannjallier.com\n";     //Pareil !!
+$headers = "From: \"SSW\"<toto@mail.com>\n"; //Taper ici votre adresse mail
+$headers .= "Reply-To: toto@mail.com\n";     //Pareil !!
 $headers .= "Content-Type: text/html; charset=\"utf-8\"";
 
 mail($to, $subject, $message, $headers);
